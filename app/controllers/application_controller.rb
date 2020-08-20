12ec0e06afe5a7fed8fadae3c9d1d06dac54d77d
TF1130 <activejob005@gmail.com>
2020-08-20 11:38:05 +0900

    nicknameカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameeters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameeters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
+  end
+  def new
+    @user =User.new
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      refirect_to action: :index
+    end
   end
 end
