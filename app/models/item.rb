c7ccd2a5a16489e5b7fe494515b545779ee0f4b4
TF1130 <activejob005@gmail.com>
2020-08-26 15:48:54 +0900

    ActiveHashのformの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :category,          presence: true
   validates :item_status,       presence: true
   validates :delivery_fee,      presence: true
-  validates :ShipCity,          presence: true
+  validates :Prefecture,        presence: true
   validates :ShipDate_Standard, presence: true
   validates :price,             presence: true #価格の範囲が、¥300~¥0,000,000の間であることの設定
   #入力された販売価格によって、非同期的に販売手数料や販売利益が変わること(jsファイルを作成して、JavaScriptを私用して実装する)
