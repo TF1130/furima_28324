1bb0af1da1e915d04aa5e90dd0ce6311b57e458d
TF1130 <activejob005@gmail.com>
2020-08-26 16:27:31 +0900

    migrationファイルにカラムidを記述

--- db/migrate/20200820054256_create_items.rb
+++ db/migrate/20200820054256_create_items.rb
@@ -1,8 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.text :text, null: false
-      t.string :title, null: false
+      t.text        :text                    , null: false
+      t.string      :title                   , null: false
+      t.string      :prefecture_id           , null: false
+      t.string      :category_id             , null: false
+      t.string      :item_status_id          , null: false
+      t.string      :delivery_fee_id         , null: false
+      t.string      :shipdate_standard_id    , null: false
       t.timestamps
     end
   end
