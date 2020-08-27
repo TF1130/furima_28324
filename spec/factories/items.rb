4a38a688c20ce3abf07b1802f7fb7ca3854616c7
TF1130 <activejob005@gmail.com>
2020-08-27 17:11:53 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,17 @@
 FactoryBot.define do
   factory :item do
-    text {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
+    text { Faker::Lorem.sentence }
+    image { Faker::Lorem.sentence }
+    title { Faker::Food }
+    text { Faker::Lorem.sentence }
+    category { Faker::Lorem.sentence }
+    item_status { Faker::Lorem.sentence }
+    delivery_fee { Faker::Lorem.sentence }
+    prefecture { Faker::Address.state }
+    shipdate_standard { Faker::Lorem.sentence }
+    price { Faker::Lorem.sentence }
     association :user 
   end
 end
+
+
