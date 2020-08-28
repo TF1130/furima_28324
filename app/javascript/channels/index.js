dc82de3bd8d255197b4051ba56dbb51dd3d57917
TF1130 <activejob005@gmail.com>
2020-08-28 22:05:08 +0900

    販売価格、販売手数料、販売利益の非同期実装

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,22 +4,3 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-function profit(){
-  const item1 = document.getElementById("item-price")
-  const item2 = document.getElementById("add-tax-price")
-  const item3 = document.getElementById("profit")
-
-item1.addEventListener("change", function(){
-  let price = item1.value;
-  function tax(){
-    return item2 = price * 0.10;
-  }
-  function tax(){
-    return item3 = price * 0.90;
-  }
-  var price = tax(price);
-  var price = Math.round(price);
-
-})
-}
-window.addEventListener("load", profit);
