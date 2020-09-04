d4b181a797f4bd0391520f3ddfba0b8ce8e6f2c9
TF1130 <activejob005@gmail.com>
2020-09-04 20:29:22 +0900

    order_locationモデルの単体テストコード

---
+++ spec/models/order_location_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe OrderLocation, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @order_location = FactoryBot.build(:order_location)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@order_location).to be_valid
+    end
+    it 'postcodeが空だと保存できないこと' do
+      @order_location.postcode = nil
+      @order_location.valid?
+      expect(@order_location.errors.full_messages).to include("Postcode can't be blank")
+    end
+    it 'postcodeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      @order_location.postcode = 'aaa'
+      @order_location.valid?
+      expect(@order_location.errors.full_messages).to include("Postcode is invalid")
+    end
+    it 'prefecture_idを選択していないと保存できないこと' do
+      @order_location.prefecture_id = 0
+      @order_location.valid?
+      expect(@order_location.errors.full_messages).to include("Prefecture must be other than 0")
+    end
+    it 'cityが空だと保存できないこと' do
+      @order_location.city = nil
+      @order_location.valid?
+      expect(@order_location.errors.full_messages).to include("City can't be blank")
+    end
+    it 'blockが空だと保存できないこと' do
+      @order_location.block = nil
+      @order_location.valid?
+      expect(@order_location.errors.full_messages).to include("Block can't be blank")
+    end
+    it 'buildingは空でも保存できること' do
+      @order_location.building = ''
+      expect(@order_location).to be_valid
+    end
+  end
+end
