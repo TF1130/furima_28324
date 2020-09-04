1e3baa1a5eafee052dc089ebc7dedd60529104d7
TF1130 <activejob005@gmail.com>
2020-09-04 20:35:52 +0900

    Formオブジェクトのjsファイル追記

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,17 +3,19 @@ const pay = () => {
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
- 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
  
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("order_location[number]"),
+      cvc: formData.get("order_location[cvc]"),
+      exp_month: formData.get("order_location[exp_month]"),
+      exp_year: `20${formData.get("order_location[exp_year]")}`,
     };
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
+    console.log(status)
+    console.table(response)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
