99f578489ed046031c00abd601a110b3eeb2d997
TF1130 <activejob005@gmail.com>
2020-09-06 18:44:25 +0900

    validatesの添削

--- app/forms/order_location.rb
+++ app/forms/order_location.rb
@@ -3,7 +3,6 @@ class OrderLocation
   attr_accessor :token, :postcode,
                 :prefecture_id, :city, :block, :phone_number,
                 :order_id, :building, :item_id, :user_id
-  validates :building, absence: true
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
