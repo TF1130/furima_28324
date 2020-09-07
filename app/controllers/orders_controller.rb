9bebf64c6f061631e197438ce9bc55a0736ff999
TF1130 <activejob005@gmail.com>
2020-09-07 12:37:54 +0900

    order_controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
+
 class OrdersController < ApplicationController
   before_action :move_to_item_orders, only: [:index]
   before_action :set_item, only: [:index, :create, :new]
   before_action :done, only: [:index]
-  # 出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること
+  #出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移すること
   def index
     @item = Item.find_by(id: params[:item_id])
     @order_location = OrderLocation.new
@@ -18,37 +19,36 @@ class OrdersController < ApplicationController
     if @order_location.valid?
       pay_item
       @order_location.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render 'index'
     end
   end
-
+  
   private
 
   def order_params
-    params.require(:order_location).permit(:price, :postcode,
-                                           :prefecture_id, :city, :block, :phone_number,
-                                           :building, :order_id).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id, order_id: params[:order_id])
+    params.require(:order_location).permit(:price,:postcode,
+      :prefecture_id, :city, :block, :phone_number,
+      :building, :order_id).merge(token: params[:token],item_id: params[:item_id], user_id: current_user.id ,order_id: params[:order_id])
   end
 
   def set_item
     @item = Item.find_by(id: params[:id])
   end
+  
 
   def token
     :token
   end
-
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]    # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @item.price, # 商品の値段
-      card: params[:token], # カードトークン
-      currency: 'jpy'                 # 通貨の種類(日本円)
+      amount: @item.price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
     )
   end
-
   def user
     @user = User.find_by(id: params[:user_id])
   end
@@ -56,7 +56,9 @@ class OrdersController < ApplicationController
   def move_to_item_orders
     @item = Item.find_by(id: params[:item_id])
     @user = User.find_by(id: params[:user_id])
-    redirect_to root_path unless user_signed_in? && current_user
+    unless user_signed_in? && current_user
+      redirect_to root_path
+    end
   end
 
   def done
